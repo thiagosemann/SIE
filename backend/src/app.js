0def4b28f634ad67eb766eb3ef5ae43689414bda
diedicaeaux4 <134411116+diedicaeaux4@users.noreply.github.com>
2023-05-24 15:09:16 -0300

    Adicionado webhook do git

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { execSync } = require('child_process'); // para executar comandos shell
 
 const router = require('./router');
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
+// adicionando o endpoint de webhook
+app.post('/webhook', (req, res) => {
+  // Aqui você pode verificar a assinatura do webhook para garantir que a solicitação é legítima
+
+  // Puxe as últimas mudanças do repositório Git
+  execSync('git pull', { stdio: 'inherit' });
+
+  // Reinicie o servidor
+  // Seu método de reiniciar o servidor dependerá de como você está gerenciando seu servidor Node.js
+
+  res.sendStatus(200);  // Responda ao webhook para que o Git saiba que ele foi recebido corretamente
+});
+
 module.exports = app;
-
-
