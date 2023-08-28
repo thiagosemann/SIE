8584623d188c8c4026f53f7fe970451581067ecd
diedicaeaux4 <thiago_semann@hotmail.com>
2023-08-28 16:43:38 -0300

    Feito

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 const connection = mysql.createPool({
-    host: "10.121.60.193",
+    host: "10.121.61.9",
     user: "root",
     password: "senha123",
     database: "SIE"
